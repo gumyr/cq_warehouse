3a41ea72f4be28dec7694320d0463fa5eb41095c
Roger Maitland <gumyr9@gmail.com>
2022-01-03 16:01:41 -0500

    Added projecting on solid and non-planar faces

--- src/cq_warehouse/wrap_text.py
+++ src/cq_warehouse/wrap_text.py
@@ -1,7 +1,7 @@
 from math import pi, sin, cos, sqrt
 import cadquery as cq
 from cadquery import Vector, Shape
-from typing import Optional, Literal
+from typing import Optional, Literal, Union
 from cadquery.occ_impl.shapes import edgesToWires
 import timeit
 
@@ -12,13 +12,73 @@ from OCP.Font import (
     Font_FA_Bold,
     Font_SystemFont,
 )
+from OCP.BRepBuilderAPI import BRepBuilderAPI_MakeFace
 from OCP.TCollection import TCollection_AsciiString
 from OCP.StdPrs import StdPrs_BRepFont, StdPrs_BRepTextBuilder as Font_BRepTextBuilder
 from OCP.NCollection import NCollection_Utf8String
 from OCP.ShapeAnalysis import ShapeAnalysis_FreeBounds
 from OCP.TopTools import TopTools_HSequenceOfShape
 from OCP.BRepOffset import BRepOffset_MakeOffset, BRepOffset_Skin
-from OCP.GeomAbs import GeomAbs_Intersection
+from OCP.BRepProj import BRepProj_Projection
+from OCP.gp import gp_Pnt, gp_Dir
+from OCP.GeomAbs import (
+    GeomAbs_Shape,
+    GeomAbs_C0,
+    GeomAbs_Intersection,
+    GeomAbs_JoinType,
+)
+from OCP.BRepOffsetAPI import BRepOffsetAPI_MakeFilling
+
+
+def _hexArray(
+    self,
+    diagonal: float,
+    xCount: int,
+    yCount: int,
+    center: Union[bool, tuple[bool, bool]] = True,
+):
+    """
+    Creates an array of points and pushes them onto the stack.
+    If you want to position the array at another point, create another workplane
+    that is shifted to the position you would like to use as a reference
+
+    :param xSpacing: spacing between points in the x direction ( must be > 0)
+    :param ySpacing: spacing between points in the y direction ( must be > 0)
+    :param xCount: number of points ( > 0 )
+    :param yCount: number of points ( > 0 )
+    :param center: If True, the array will be centered around the workplane center.
+      If False, the lower corner will be on the reference point and the array will
+      extend in the positive x and y directions. Can also use a 2-tuple to specify
+      centering along each axis.
+    """
+    xSpacing = 3 * diagonal / 4
+    ySpacing = diagonal * sqrt(3) / 2
+    if xSpacing <= 0 or ySpacing <= 0 or xCount < 1 or yCount < 1:
+        raise ValueError("Spacing and count must be > 0 ")
+
+    if isinstance(center, bool):
+        center = (center, center)
+
+    lpoints = []  # coordinates relative to bottom left point
+    for x in range(0, xCount, 2):
+        for y in range(yCount):
+            lpoints.append(Vector(xSpacing * x, ySpacing * y + ySpacing / 2))
+    for x in range(1, xCount, 2):
+        for y in range(yCount):
+            lpoints.append(Vector(xSpacing * x, ySpacing * y + ySpacing))
+
+    # shift points down and left relative to origin if requested
+    offset = Vector()
+    if center[0]:
+        offset += Vector(-xSpacing * (xCount - 1) * 0.5, 0)
+    if center[1]:
+        offset += Vector(0, -ySpacing * (yCount - 1) * 0.5)
+    lpoints = [x + offset for x in lpoints]
+
+    return self.pushPoints(lpoints)
+
+
+cq.Workplane.hexArray = _hexArray
 
 
 def sortWires(wires: list[cq.Wire]) -> tuple[list[cq.Wire], list[cq.Wire]]:
@@ -165,20 +225,87 @@ cq.Face.thicken = _thicken
 
 def _flipNormal(self) -> int:
     """Checks the direction of the normal of a cylindrical face"""
-    position = self.outerWire().positionAt(0)
+    position = self.wrapped.outerWire().positionAt(0)
     normal = self.normalAt()
-    # return False if normal.dot(position) > 0 else True
     return 1 if normal.dot(position) > 0 else -1
 
 
 cq.Face.flipNormal = _flipNormal
 
 
-def makeTextOnCylinder(
+def _projectWireOnSolid(
+    self: cq.Wire, solid_object: cq.Solid, direction: cq.Vector
+) -> list[cq.Wire]:
+    projection_object = BRepProj_Projection(
+        self.wrapped,
+        cq.Shape.cast(solid_object.wrapped).wrapped,
+        gp_Dir(*direction.toTuple()),
+    )
+    # A Compound could be generated but it seems less useful
+    # output_wires = projection_object.Shape()
+
+    # Generate a list of the projected wires
+    output_wires = []
+    while projection_object.More():
+        output_wires.append(cq.Wire(projection_object.Current()))
+        projection_object.Next()
+
+    return output_wires
+
+
+cq.Wire.projectWireOnSolid = _projectWireOnSolid
+
+
+def makeNonPlanarFace(
+    exterior: Union[cq.Wire, list[cq.Edge]],
+    surfacePoints: list[cq.Vector] = None,
+    interiorWires: list[cq.Wire] = None,
+) -> cq.Face:
+    """Create a potentally non-planar face bounded by exterior (wire or edges),
+    optionally refined by surfacePoints with optional holes defined by interiorWires"""
+
+    # First, create the non-planar surface
+    surface = BRepOffsetAPI_MakeFilling(
+        Degree=3,
+        NbPtsOnCur=15,
+        NbIter=2,
+        Anisotropie=False,
+        Tol2d=0.00001,
+        Tol3d=0.0001,
+        TolAng=0.01,
+        TolCurv=0.1,
+        MaxDeg=8,
+        MaxSegments=9,
+    )
+    if isinstance(exterior, cq.Wire):
+        outside_edges = exterior.Edges()
+    else:
+        outside_edges = [e.Edge() for e in exterior]
+    for edge in outside_edges:
+        surface.Add(edge.wrapped, GeomAbs_C0)
+    if surfacePoints:
+        for pt in surfacePoints:
+            surface.Add(gp_Pnt(*pt.toTuple()))
+    surface.Build()
+
+    # Next, add wires that define interior holes - note these wires must be entirely interior
+
+    # surface_face = cq.Face(surface.Shape()).fix()
+    surface_face = cq.Face(surface.Shape())
+    if interiorWires:
+        makeface_object = BRepBuilderAPI_MakeFace(surface_face.wrapped)
+        for w in interiorWires:
+            makeface_object.Add(w.wrapped)
+        surface_face = cq.Face(makeface_object.Face()).fix()
+    return surface_face
+
+
+def makeTextOnSolid(
     text: str,
     size: float,
-    radius: float,
     thickness: float,
+    solid_object: cq.Solid,
+    direction: cq.Vector,
     font: str = "Arial",
     fontPath: Optional[str] = None,
     kind: Literal["regular", "bold", "italic"] = "regular",
@@ -198,71 +325,112 @@ def makeTextOnCylinder(
         valign=valign,
     )
 
-    # The character insides need to be removed from the outside but their
-    # normals may not be in the same direction as the outside so increase
-    # the thickness and thicken the face in both directions
-    inside_thickness = min(0.99 * radius, 2 * thickness)
     solid_char_list = []
     # For each character in the text
     for outside_wires, inside_wires in text_wire_list:
-        # if inside_wires:
-        #     projected_inside_wires = [w.mapToCylinder(radius) for w in inside_wires]
-        #     inside_faces = [
-        #         cq.Face.makeNSidedSurface(w.Edges(), []) for w in projected_inside_wires
-        #     ]
-        #     reverse_wire = [f.flipNormal for f in inside_faces]
-        #     corrected_projected_inside_wires = []
-        #     for i, w in enumerate(projected_inside_wires):
-        #         if reverse_wire[i]:
-        #             corrected_projected_inside_wires.append(
-        #                 cq.Wire(w.wrapped.Reversed())
-        #             )
-        #         else:
-        #             corrected_projected_inside_wires.append(w)
-
-        projected_outside_wires = [w.mapToCylinder(radius) for w in outside_wires]
-        # projected_inside_wires = [w.mapToCylinder(radius) for w in inside_wires]
+        projected_outside_wires = []
+        for w in outside_wires:
+            projected_outside_wires.extend(
+                w.projectWireOnSolid(solid_object, direction)
+            )
+        if inside_wires:
+            projected_inside_wires = []
+            for w in inside_wires:
+                projected_inside_wires.extend(
+                    w.projectWireOnSolid(solid_object, direction)
+                )
         outside_faces = [
-            cq.Face.makeNSidedSurface(w.Edges(), [])
-            # for w in projected_outside_wires + corrected_projected_inside_wires
+            makeNonPlanarFace(w, interiorWires=projected_inside_wires)
             for w in projected_outside_wires
         ]
-        outside_solid = cq.Compound.makeCompound(
-            [f.thicken(f.flipNormal() * thickness) for f in outside_faces]
-        )
-        if inside_wires:
-            projected_inside_wires = [w.mapToCylinder(radius) for w in inside_wires]
-            inside_faces = [
-                cq.Face.makeNSidedSurface(w.Edges(), []) for w in projected_inside_wires
-            ]
-            # inside_solid = cq.Compound.makeCompound(
-            #     [f.thicken(f.flipNormal() * 2 * thickness) for f in inside_faces]
-            # )
-            inside_solid = cq.Compound.makeCompound(
-                [
-                    f.thicken(inside_thickness).fuse(f.thicken(-inside_thickness))
-                    for f in inside_faces
-                ]
-            )
-            # outside_solid = inside_solid
-            outside_solid = outside_solid.cut(inside_solid)
-        solid_char_list.append(outside_solid)
+        print(f"{type(outside_faces[0])=}")
+        # outside_solid = cq.Compound.makeCompound(
+        #     # [f.thicken(f.flipNormal() * thickness) for f in outside_faces]
+        #     [f.thicken(thickness) for f in outside_faces]
+        # )
 
-    return cq.Compound.makeCompound(solid_char_list)
+        # if inside_wires:
+        #     projected_inside_wires = []
+        #     for w in inside_wires:
+        #         projected_inside_wires.extend(
+        #             w.projectWireOnSolid(solid_object, direction)
+        #         )
+        #     all_edges = []
+        #     for w in projected_outside_wires:
+        #         all_edges.extend(w.Edges())
+        #     for w in projected_inside_wires:
+        #         all_edges.extend(w.Edges())
+        #     face = cq.Face.makeNSidedSurface(all_edges, [])
+        # inside_faces = [
+        #     cq.Face.makeNSidedSurface(w.Edges(), []) for w in projected_inside_wires
+        # ]
+        # inside_solid = cq.Compound.makeCompound(
+        #     [
+        #         f.thicken(inside_thickness).fuse(f.thicken(-inside_thickness))
+        #         for f in inside_faces
+        #     ]
+        # )
+        # # outside_solid = inside_solid
+        # outside_solid = outside_solid.cut(inside_solid)
+        # solid_char_list.append(outside_solid)
+    return outside_faces[1]
+    # return cq.Compound.makeCompound(solid_char_list)
 
 
-cyl = cq.Solid.makeCylinder(10, 10)
+sphere_solid = cq.Solid.makeSphere(50, angleDegrees1=-90)
 
-starttime = timeit.default_timer()
-test_text = makeTextOnCylinder(
-    "Beingφθ⌀",
-    size=10,
-    radius=10,
-    thickness=1,
-    font="Serif",
-    fontPath="/usr/share/fonts/truetype/freefont",
-)
-print(f"The time difference is: {timeit.default_timer() - starttime:0.2f}s")
+# starttime = timeit.default_timer()
+# test_text = makeTextOnSolid(
+#     # "Beingφθ⌀",
+#     "e",
+#     size=10,
+#     thickness=1,
+#     solid_object=sphere_solid,
+#     direction=cq.Vector(0, 0, 1),
+#     font="Serif",
+#     fontPath="/usr/share/fonts/truetype/freefont",
+#     halign="center",
+# )
+# print(f"The time difference is: {timeit.default_timer() - starttime:0.2f}s")
+
+(outer_e, inner_e) = makeTextWires("e", 10)[0]
+print(type(outer_e[0]))
+print(type(inner_e), len(inner_e), type(inner_e[0]))
+e_face_outer = makeNonPlanarFace(outer_e[0])
+e_face_inner = makeNonPlanarFace(inner_e[0])
+e_face = makeNonPlanarFace(outer_e[0], interiorWires=inner_e)
+e_solid = e_face.thicken(1)
+# r = 10
+# cyl = cq.Solid.makeCylinder(r, 40)
+# starttime = timeit.default_timer()
+# c = 2 * pi * r
+# d = 4 * (c / 10) / 3
+# pattern_wires = (
+#     cq.Workplane("XY")
+#     .hexArray(d, 10, 5, center=(True, False))
+#     .polygon(6, 0.8 * d)
+#     .wires()
+#     .vals()
+# )
+# mapped_wires = [w.mapToCylinder(r) for w in pattern_wires]
+# mapped_faces = [cq.Face.makeNSidedSurface(w.Edges(), []) for w in mapped_wires]
+# pos_pattern = cq.Compound.makeCompound(
+#     [f.thicken(f.flipNormal() * 1) for f in mapped_faces]
+# )
+# pos_patterned_cylinder = cyl.fuse(pos_pattern, glue=True)
+# print(f"The time difference is: {timeit.default_timer() - starttime:0.2f}s")
+
 if "show_object" in locals():
-    show_object(test_text, name="test_text")
-    show_object(cyl, name="cyl")
+    # show_object(test_text, name="test_text")
+    # show_object(sphere_solid, name="sphere_solid")
+    # show_object(cyl, name="cyl")
+    # show_object(pattern_wires, name="pattern_wires")
+    # show_object(mapped_wires, name="mapped_wires")
+    # show_object(pos_pattern, name="pos_pattern")
+    # show_object(pos_patterned_cylinder, name="pos_patterned_cylinder")
+    show_object(outer_e, name="outer_e")
+    show_object(inner_e, name="inner_e")
+    show_object(e_face_outer, name="e_face_outer")
+    show_object(e_face_inner, name="e_face_inner")
+    show_object(e_face, name="e_face")
+    show_object(e_solid, name="e_solid")
