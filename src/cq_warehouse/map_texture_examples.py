da6f85469191d504592e43d0666631eddcec751e
Roger Maitland <gumyr9@gmail.com>
2022-01-08 11:34:23 -0500

    Canadian Flag

--- src/cq_warehouse/map_texture_examples.py
+++ src/cq_warehouse/map_texture_examples.py
@@ -95,21 +95,25 @@ elif example == 3:
 
 elif example == 4:
     """Example 4 - Mapping A Face on Sphere"""
+    starttime = timeit.default_timer()
     projection_direction = cq.Vector(0, 0, 1)
 
     square = cq.Workplane("XY").rect(20, 20).extrude(1).faces("<Z").val()
     square_projected = square.projectToSolid(sphere, projection_direction)
     square_solids = cq.Compound.makeCompound([f.thicken(2) for f in square_projected])
+    print(f"Example #{example} time: {timeit.default_timer() - starttime:0.2f}s")
     if "show_object" in locals():
         show_object(sphere, name="sphere_solid", options={"alpha": 0.8})
         show_object(square_solids, name="square_solids")
 
 elif example == 5:
-    projection_direction = cq.Vector(0, 0, 1)
-    projection_plane = cq.Plane.named("XY")
+    """Example 5 - A Canadian Flag blowing in the wind"""
+    starttime = timeit.default_timer()
 
+    # Canadian Flags have a 2:1 aspect ratio
     height = 50
-    width = 100
+    width = 2 * height
+    wave_amplitude = 3
 
     def surface(amplitude, u, v):
         """Calculate the surface displacement of the flag at a given position"""
@@ -117,130 +121,118 @@ elif example == 5:
             1.1 * pi * v
         )
 
+    # Note that the surface to project on must be a little larger than the faces
+    # being projected onto it to create valid projected faces
     flag_surface = (
         cq.Workplane("XY")
         .parametricSurface(
             lambda u, v: cq.Vector(
-                v * width,
-                u * height,
-                height * surface(2, u, v),
+                v * width * 1.01,
+                u * height * 1.01,
+                height * surface(wave_amplitude, u, v) / 2,
             ),
             N=40,
         )
-        .thicken(1)
+        .thicken(0.5)
+        .translate((-width * 0.005, -height * 0.005, 0))
         .val()
     )
-    # Canadian Flag raw dimensions 9600x4800
-    west_field = cq.Workplane("XY").rect(2400, 4800, centered=False).wires().val()
-    east_field = (
+    west_field = (
         cq.Workplane("XY")
-        .center(7200, 0)
-        .rect(2400, 4800, centered=False)
+        .center(-1, 0)
+        .rect(0.5, 1, centered=False)
         .wires()
         .val()
+        .scale(height)
+    )
+    east_field = west_field.mirror("YZ")
+    center_field = (
+        cq.Workplane("XY")
+        .center(-0.5, 0)
+        .rect(1, 1, centered=False)
+        .wires()
+        .val()
+        .scale(height)
     )
     maple_leaf = (
         cq.Workplane("XY")
-        .moveTo(4890, 4430)
-        .lineTo(4845, 3567)
-        .radiusArc((4956, 3469), -130)
-        .lineTo(5815, 3620)
-        .lineTo(5699, 3300)
-        .radiusArc((5719, 3227), -130)
-        .lineTo(6660, 2465)
-        .lineTo(6448, 2366)
-        .radiusArc((6414, 2287), -130)
-        .lineTo(6600, 1715)
-        .lineTo(6058, 1830)
-        .radiusArc((5985, 1792), -130)
-        .lineTo(5880, 1545)
-        .lineTo(5457, 1999)
-        .radiusArc((5346, 1942), -130)
-        .lineTo(5550, 890)
-        .lineTo(5223, 1079)
-        .radiusArc((5132, 1052), -130)
-        .lineTo(4800, 400)
-        .lineTo(4468, 1052)
-        .radiusArc((4377, 1079), -130)
-        .lineTo(4050, 890)
-        .lineTo(4254, 1942)
-        .radiusArc((4143, 1999), -130)
-        .lineTo(3720, 1545)
-        .lineTo(3615, 1792)
-        .radiusArc((3542, 1830), -130)
-        .lineTo(3000, 1715)
-        .lineTo(3186, 2287)
-        .radiusArc((3152, 2366), -130)
-        .lineTo(2940, 2465)
-        .lineTo(3881, 3227)
-        .radiusArc((3901, 3300), -130)
-        .lineTo(3785, 3620)
-        .lineTo(4644, 3469)
-        .radiusArc((4755, 3567), -130)
-        .lineTo(4710, 4430)
-        .close()
-        .mirror("XZ")
-        .translate((0, 4800, 0))
+        .moveTo(0.0000, 0.0771)
+        .lineTo(0.0187, 0.0771)
+        .lineTo(0.0094, 0.2569)
+        .radiusArc((0.0325, 0.2773), 0.0271)
+        .lineTo(0.2115, 0.2458)
+        .lineTo(0.1873, 0.3125)
+        .radiusArc((0.1915, 0.3277), 0.0271)
+        .lineTo(0.3875, 0.4865)
+        .lineTo(0.3433, 0.5071)
+        .radiusArc((0.3362, 0.5235), 0.0271)
+        .lineTo(0.375, 0.6427)
+        .lineTo(0.2621, 0.6188)
+        .radiusArc((0.2469, 0.6267), 0.0271)
+        .lineTo(0.225, 0.6781)
+        .lineTo(0.1369, 0.5835)
+        .radiusArc((0.1138, 0.5954), 0.0271)
+        .lineTo(0.1562, 0.8146)
+        .lineTo(0.0881, 0.7752)
+        .radiusArc((0.0692, 0.7808), 0.0271)
+        .lineTo(0.0, 0.9167)
+        .mirrorY()
         .wires()
         .val()
+        .scale(height)
     )
     flag_faces = [
-        cq.Face.makeFromWires(w.scale(height / 4800), [])
+        cq.Face.makeFromWires(w, []).translate(cq.Vector(width / 2, 0, 30))
         for w in [west_field, maple_leaf, east_field]
     ]
-    for f in flag_faces:
-        print(f"{f.isValid()}")
-    maple_on_sphere = (
-        flag_faces[1]
-        .translate((-width / 2, -height / 2, 0))
-        .projectToSolid(flag_surface, cq.Vector(0, 0, -1))
+    flag_faces.append(
+        cq.Face.makeFromWires(center_field, [maple_leaf]).translate(
+            cq.Vector(width / 2, 0, 30)
+        )
     )
-    # projected_flag_faces = [
-    #     # f.projectToSolid(flag_surface, cq.Vector(0, 0, -1)) for f in flag_faces
-    #     f.projectToSolid(sphere, cq.Vector(0, 0, -1))[0]
-    #     for f in flag_faces
-    # ]
-    # flag_image = cq.Compound.makeCompound(
-    #     [f.thicken(3).translate((0, 0, -1.5)) for f in projected_flag_faces]
-    # )
+    projected_flag_faces = [
+        f.projectToSolid(flag_surface, cq.Vector(0, 0, -1))[FRONT] for f in flag_faces
+    ]
+    flag_parts = [f.thicken(1.5, cq.Vector(0, 0, 1)) for f in projected_flag_faces]
+    print(f"Example #{example} time: {timeit.default_timer() - starttime:0.2f}s")
 
     if "show_object" in locals():
-        show_object(sphere, name="sphere", options={"alpha": 0.8})
-        # show_object(flag_surface, name="flag_surface", options={"alpha": 0.8})
-        show_object(flag_faces[1], name="flag_faces")
-        show_object(maple_on_sphere, name="maple_on_sphere")
-        # show_object(projected_flag_faces, name="projected_flag_faces")
-        # show_object(flag_image, name="flag_image")
+        show_object(flag_surface, name="flag_surface", options={"alpha": 0.8})
+        show_object(
+            flag_parts[0:-1], name="flag_red_parts", options={"color": (255, 0, 0)}
+        )
+        show_object(
+            flag_parts[-1], name="flag_white_part", options={"color": (255, 255, 255)}
+        )
 
 else:
-    """Example 6 - Compound Solid"""
-    # complex_solid = (
-    #     cq.Workplane("XY")
-    #     .rect(100, 50)
-    #     .rect(50, 100)
-    #     .extrude(50, both=True)
-    #     .edges("|Z")
-    #     .fillet(10)
-    # )
+    """Example 6 - Compound Solid - under construction"""
+    compound_solid = (
+        cq.Workplane("XY")
+        .rect(100, 50)
+        .rect(50, 100)
+        .extrude(50, both=True)
+        .edges("|Z")
+        .fillet(10)
+    )
 
-    # complex_solid_faces = complex_solid.faces().vals()
-    # print(f"{len(complex_solid_faces)=}")
-    # text_path = complex_solid.section().wires().val()
-    # for i, f in enumerate(complex_solid_faces):
-    #     print(f"{i}:{f.isInside(text_path.positionAt(0))}")
-    # print(type(text_path))
+    compound_solid_faces = compound_solid.faces().vals()
+    print(f"{len(compound_solid_faces)=}")
+    text_path = compound_solid.section().wires().val()
+    for i, f in enumerate(compound_solid_faces):
+        print(f"{i}:{f.isInside(text_path.positionAt(0))}")
+    print(type(text_path))
 
-    # text_on_complex = textOnSolid(
+    # text_on_compound = textOnSolid(
     #     txt="The quick brown fox jumped over the lazy dog",
     #     fontsize=10,
     #     distance=5,
     #     path=text_path,
     #     start=0,
-    #     solid_object=complex_solid.val(),
+    #     solid_object=compound_solid.val(),
     # )
 
-    # if "show_object" in locals():
-    #     show_object(xz_text_faces, name="text_faces")
-    # show_object(complex_solid, name="complex_solid", options={"alpha": 0.8})
-    # show_object(text_path, name="text_path")
-    # show_object(text_on_complex, name="text_on_complex")
+    if "show_object" in locals():
+        show_object(compound_solid, name="compound_solid", options={"alpha": 0.8})
+        show_object(text_path, name="text_path")
+        show_object(text_on_compound, name="text_on_compound")
